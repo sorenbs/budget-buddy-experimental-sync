9253bfc6d06c1e36e2da76473c979cdcd435dbfc
Keith Kurak <keith.kurak@gmail.com>
2024-06-05 12:32:03 -0400

    avoid live-reloading client when server DB changes

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,11 @@
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+
 const { getDefaultConfig } = require("expo/metro-config");
 
 const defaultConfig = getDefaultConfig(__dirname);
 
 defaultConfig.resolver.assetExts.push("db");
+
+defaultConfig.resolver.blockList = exclusionList([/server\/dbs\/.*/])
 
 module.exports = defaultConfig;
